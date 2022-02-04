093a6692a610aefaecd5381548ac5bea55cd2d68
harishm <harishmahesh1911@gmail.com>
2022-02-04 19:58:08 +0530

    updated for deployment

--- backend/server.js
+++ backend/server.js
@@ -24,11 +24,11 @@ const server = app.listen(process.env.PORT || 5000, () => {
 });
 
 // --------------------------deployment------------------------------
-
-app.use(express.static(path.join(__dirname, "/frontend")));
+__dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/frontend/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
 // --------------------------deployment------------------------------
 
